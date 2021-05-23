f36c551bc07108751a943623898c32ee8bbe0ac2
usman-kakakhel <usman.kakakhel@ug.bilkent.edu.tr>
2021-05-23 19:29:22 +0000

    Small changes

---
+++ download_weights.py
@@ -0,0 +1,5 @@
+from google_drive_downloader import GoogleDriveDownloader as gdd
+import os
+
+gdd.download_file_from_google_drive(file_id='10f8mj8OUEQ1_YASCktXY6v9rRyMDZMLB', dest_path='./model_final.pth')
+
